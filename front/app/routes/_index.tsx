1b6192715174c066db2460575d325adec943c3db
Nath9666 <nathan.morel@efrei.net>
2024-08-05 17:16:04 +0200

    appel fonctionel

--- front/app/routes/_index.tsx
+++ front/app/routes/_index.tsx
@@ -1,6 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
-import { Link } from "@remix-run/react";
-import Dropzone from "../components/dropZone";
+import { useEffect } from 'react';
 
 export const meta: MetaFunction = () => {
   return [
@@ -10,6 +9,20 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://127.0.0.1:5000/reconize');
+        const data = await response.json();
+        console.log(data);
+      } catch (error) {
+        console.error('Erreur lors de la récupération des données:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="font-sans p-4">
       <h1 className="text-2xl font-bold">Bienvenue à Belote classement</h1>
